db8317ca9dfb0018e37ec84a05cf8efa74d53a05
Tom Barnes <tomtombarnes2@gmail.com>
2022-06-07 16:03:12 +0100

    Files updated at 07/06/2022 - 16:03

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import time
 _start = time.time()
 
+import prime_sieve
+import sys
 import random
 import pickle
 
@@ -15,23 +17,26 @@ def sieve(n):
                 marker[j] = 0
     return primes
 
-def segmented_sieve(n, primes_step=1):
+def segmented_sieve(n, step_target=100):
     primes = [2, 3, 5, 7]
     start_idx = 1
     count = 1
+    step = 1
     while True:   
-        print(f"Segment: {count}", end="\r")
-        step = min(primes_step, len(primes) - 1 - start_idx)
+        step = min(step_target, len(primes) - 1 - start_idx)
         p, q = primes[start_idx], primes[start_idx + step]
         multiples = primes[:start_idx + 1]
-        start, end = p * p, q * q
+        start, end = p * p, q * q - 1
+        length = end - start + 1
         segment = [i for i in range(start, end)]
-        marker = [1 for i in range(start, end)]
+        if "-v" in sys.argv:
+            print(f"Segment size: {length}", end="\r")
+        marker = [1 for i in range(length)]
         marker[0] = 0
 
         for m in multiples:
             offset = m * (segment[0] // m) + (0 if segment[0] % m == 0 else m) - segment[0]
-            for i in range(0, end - start, m):
+            for i in range(0, length, m):
                 try:
                     marker[i + offset] = 0
                 except IndexError:
@@ -39,7 +44,9 @@ def segmented_sieve(n, primes_step=1):
         
         primes_in_segment = [segment[i] for i in range(len(segment)) if marker[i] == 1]
         if primes_in_segment[-1] >= n:
-            primes.extend([prime for prime in primes_in_segment if prime < n])
+            for prime in primes_in_segment:
+                if prime < n:
+                    primes.append(prime)
             break
         else:
             primes.extend(primes_in_segment)
@@ -47,7 +54,7 @@ def segmented_sieve(n, primes_step=1):
         count += 1
     return primes
 
-primes = segmented_sieve(1e+8)
+primes = segmented_sieve(1e+7)
 print(f"Found {len(primes)} primes")
 with open("output.pkl", "wb") as f:
     pickle.dump(primes, f)
